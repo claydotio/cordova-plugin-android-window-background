1f7985562742a618f7f0c85a49e0dfcd0fbca82b
Austin Hallock <austin@clay.io>
2018-09-03 18:42:07 -0500

    use activity instead of AppCompatActivity

--- SplashActivity.java
+++ SplashActivity.java
@@ -1,12 +1,12 @@
 package com.openfam.windowbackground;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
